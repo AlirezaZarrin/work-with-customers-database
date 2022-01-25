3e5eee6fe0505780207e0c645798382c63717dd4
AlirezaZarrin <alirezazarrin1144@gmail.com>
2022-01-25 02:47:21 -0800

    first commit

---
+++ work with customers database.js
@@ -0,0 +1,31 @@
+let myData = [
+    {
+        name : 'ali',
+        family : 'kooshesh',
+        age : 22,
+        buy : 250,
+        mobile : 09362766356
+    },
+    {
+        name : 'reza',
+        family : 'sarv',
+        age : 40,
+        buy : 1200,
+        mobile : 0922531455
+    },
+    {
+        name : 'alireza',
+        family : 'sazkar',
+        age : 12,
+        buy : 50,
+        mobile : 93145614561
+    },
+    {
+        name : 'sara',
+        family : 'valizade',
+        age : 18,
+        buy : 100,
+        mobile : 63124512451
+    },
+    
+]
